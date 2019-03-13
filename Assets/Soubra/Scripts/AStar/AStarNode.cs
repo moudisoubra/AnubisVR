06a07f3c2edba86756cf05f9971dac6f61050870
moudisoubra <moudisoubra001@gmail.com>
2019-03-13 20:39:16 +0400

    Working on A*

---
+++ Assets/Soubra/Scripts/AStar/AStarNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarNode
+{
+
+    public Vector3 worldPosition;
+    public bool walkable;
+
+    public int gCost;
+    public int hCost;
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+        private set { }
+    }
+
+    public Vector3Int gridPosition;
+
+    public AStarNode(Vector3 worldposition)
+    {
+        worldPosition = worldposition;
+    } 
+}
