d6ef68718cc5980bd201e13ab41d78a3003437b3
ahmedhajjo <ahmad_1996@live.com>
2019-03-21 17:32:36 +0400

    BehaivourTreeHajjo

---
+++ Assets/AhmedHajjoFolder/HajjoScript/NodesHajjo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodesHajjo : MonoBehaviour
+{
+    public List<NodesHajjo> nodeList = new List<NodesHajjo>();
+
+    public enum Results { success, running, failure };
+
+
+    public virtual Results execute(BTreeHajjo Bt)
+    {
+        return Results.running;
+
+    }
+}
---
+++ Assets/AhmedHajjoFolder/HajjoScript/SelectorHajjo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorHajjo : NodesHajjo
+{
+    public override Results execute(BTreeHajjo Bt)
+    {
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            NodesHajjo.Results results = nodeList[i].execute(Bt);
+
+            if (results == Results.success)
+            {
+                Debug.Log("Selector Node Successed");
+                return Results.success;
+            }
+
+            else if (results == Results.running)
+            {
+                Debug.Log("Selector Running");
+                return Results.running;
+
+            }
+        }
+        Debug.Log("Selector Fail");
+        return Results.failure;
+
+
+
+
+
+
+    }
+
+}
