0fcf7ecf12dfc4705b2b36149e22b7f20591a564
ahmedhajjo <ahmad_1996@live.com>
2019-04-02 00:25:43 +0400

    DDijkstra's Algorithm PathFindingDone

---
+++ Assets/AhmedHajjoFolder/PathFindingHajjoDone/RunningThePath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningThePath : MonoBehaviour
+{
+
+    [SerializeField] Transform startPoint;
+    [SerializeField] Transform lastPoint;
+    bool butoonUp = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (butoonUp && Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Dijkstra msg send...");
+
+            butoonUp = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.I))
+        {
+            butoonUp = true;
+        }
+        DjisPathFindHajjo.instance.DjiPath(startPoint.position, lastPoint.position);
+    }
+}
+
