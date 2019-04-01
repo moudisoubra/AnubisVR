80791c4b15cd9339d6153b29e2f511d8f61f6337
moudisoubra <moudisoubra001@gmail.com>
2019-04-01 22:15:03 +0400

    Fixing Weapons

---
+++ Assets/Soubra/Scripts/DistanceCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCheck : MonoBehaviour
+{
+
+    public GameObject One;
+    public GameObject Two;
+        
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Vector3.Distance(One.transform.position, Two.transform.position));
+    }
+}
