62fe8ab2475698d1e3b5c476a0888c1481632649
ahmedhajjo <ahmad_1996@live.com>
2019-04-06 12:44:34 +0400

    Djistra is Done , Now working On Patroling and Chasing

--- Assets/AhmedHajjoFolder/PathFindingHajjoDone/RunningThePath.cs
+++ Assets/AhmedHajjoFolder/PathFindingHajjoDone/RunningThePath.cs
@@ -7,22 +7,123 @@ public class RunningThePath : MonoBehaviour
 
     [SerializeField] Transform startPoint;
     [SerializeField] Transform lastPoint;
-    bool butoonUp = true;
+    bool butoonUp = false;
+    [SerializeField] private int[] PathPoints;
+    private int currentPoint = 0;
+    private int lastNodeID = -1;
+
+    private bool pathCollected = false;
+
+    int goToNode;
+
+    float speed = 10f;
+    
+    bool veryNear = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (butoonUp && Input.GetKeyDown(KeyCode.I))
+        if (!pathCollected)
         {
-            Debug.Log("Dijkstra msg send...");
+            PathPoints = DjisPathFindHajjo.instance.DjiPath(transform.position, lastPoint.position);
+            pathCollected = true;
+        }
 
-            butoonUp = false;
-        }
-        else if (Input.GetKeyUp(KeyCode.I))
+        Vector3 pos = DjisPathFindHajjo.instance.allNodes[PathPoints[currentPoint]].trans.position;
+
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+
+        if (Vector3.Distance(transform.position, pos) < 0.1f)
         {
-            butoonUp = true;
+            currentPoint++;
         }
-        DjisPathFindHajjo.instance.DjiPath(startPoint.position, lastPoint.position);
+
+        if (currentPoint == PathPoints.Length)
+        {
+            pathCollected = false;
+            currentPoint = 0;
+        }
+
+/*
+        else
+        {
+            currentPoint = 0;
+        }
+*/
+
+        /*
+         *        PathPoints = DjisPathFindHajjo.instance.DjiPath(transform.position, lastPoint.position);
+               Vector3 pos = DjisPathFindHajjo.instance.allNodes[PathPoints[currentPoint]].trans.position;
+
+
+                transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+
+
+
+
+                if (Vector3.Distance(transform.position, pos) < 0.05f)
+                {
+                    lastNodeID = currentPoint;
+                    currentPoint++;
+                }
+
+
+                if(lastNodeID > 1)
+                {
+                    lastNodeID = 0;
+                    currentPoint = 0;
+                }
+
+
+
+
+         * */
+
+
+
+
+
+
+
+
+
+
+        //if (butoonUp && Input.GetKeyDown(KeyCode.I))
+        //{
+        //    Debug.Log("Dijkstra msg send...");
+
+        //    butoonUp = false;
+        //}
+
+        //else if (Input.GetKeyUp(KeyCode.I))
+        //{
+        //    butoonUp = true;
+
+
+        //}
+
+
+
+
+
+
     }
+
+
+
+
+
+    private void OnDrawGizmos()
+    {
+       
+
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 5f);
+    }
+
 }
+
 
