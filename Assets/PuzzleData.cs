13f82d26d447e404e5d655f7c6868055e2419cf1
ahmedhajjo <ahmad_1996@live.com>
2019-04-22 14:54:57 +0400

    PuzzleDone Finally

---
+++ Assets/PuzzleData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleData : MonoBehaviour
+{
+    public bool solved = false;
+    public bool facing = false;
+    public int keyType = 0;
+    public Animation anim;
+
+    private void Start()
+    {
+        
+        anim = GetComponent<Animation>();
+    }
+
+    public void Reset()
+    {
+        Debug.Log("Reseting...");
+        anim.Play("TurnPuzzleCube_Back");
+        facing = false;
+    }
+
+}
